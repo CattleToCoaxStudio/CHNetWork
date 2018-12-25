b9f1e57e74bc326f19c57b71730f7df11d45fb5d
chengyou.yang <>
2018-12-26 00:52:25 +0800

    test 1.0.0

--- CHNetWork/Classes/NSData+CHEncrypt.m
+++ CHNetWork/Classes/NSData+CHEncrypt.m
@@ -6,8 +6,6 @@
 //
 
 #import "NSData+CHEncrypt.h"
-#pragma GCC diagnostic ignored "-Wselector"
-#import <Availability.h>
 #import <CommonCrypto/CommonCryptor.h>
 
 @implementation NSData (CHEncrypt)
